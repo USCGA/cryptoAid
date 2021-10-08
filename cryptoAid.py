278f0bfcd1e40d39e4e7cf7df09737fef8a90e9c
Joram Stith <joramstith@gmail.com>
2021-10-08 14:40:09 -0400

    lists out functions when imported

--- cryptoAid.py
+++ cryptoAid.py
@@ -26,3 +26,6 @@ def primRoots(modulo):
     required_set = {num for num in range(1, modulo) if gcd(num, modulo) }
     return [g for g in range(1, modulo) if required_set == {pow(g, powers, modulo)
             for powers in range(1, modulo)}]
+
+
+print("is_coprime, gcd, modInverse, totient, primRoots")
